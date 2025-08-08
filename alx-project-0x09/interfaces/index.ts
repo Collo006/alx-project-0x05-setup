04902c6da55043c82c635bfdecd2352295937b83
Collins Kipruto <collinskipruto036@gmail.com>
2025-08-08 14:39:07 +0300

    submit

--- alx-project-0x09/interfaces/index.ts
+++ alx-project-0x09/interfaces/index.ts
@@ -1,16 +1,2 @@
-import { ReactNode } from "react";
-
-export interface ReactComponentProps {
-  children: ReactNode
-}
-
-
-export interface GeneratedImageProps {
-  imageUrl: string
-  prompt: string
-  width?: string
-  height?: string
-  action: (imagePath: string) => void
-}
-
-export type ImageProps = Pick<GeneratedImageProps, "imageUrl" | "prompt">
+export const WIDTH = 512
+export const HEIGHT = 512
